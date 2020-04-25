7dd31e748f259276d40eaa874c50737be8894a16
Stephanie Ballard <user.name>
2020-04-25 01:00:21 -0700

    sign up working with firebase

--- FoodTruck/ViewControllers/SignUpViewController.swift
+++ FoodTruck/ViewControllers/SignUpViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import FirebaseAuth
+
+typealias completion = Result<User?, Error>
 
 class SignUpViewController: UIViewController {
 
+    var handle: AuthStateDidChangeListenerHandle?
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var retypePasswordTextField: UITextField!
@@ -20,7 +25,36 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+//        Auth.auth().removeStateDidChangeListener(handle!)
+        
+    }
+    
+    
     @IBAction func signupButtonTapped(_ sender: UIButton) {
+        guard
+            let email = emailTextField.text,
+            let password = passwordTextField.text
+            else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { auth, error in
+            if let error = error {
+                print("Error creating account: \(error)")
+            } else {
+                if let auth = auth {
+                    print("Success!!!")
+                    print(auth.user.uid)
+                    print(auth.user.email as Any)
+                }
+            }
+        }
     }
     
     /*
@@ -32,5 +66,13 @@ class SignUpViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func createAnAccount(email: String, password: String, completion: @escaping(completion) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                NSLog("Error creating User: \(error)")
+                return
+            }
+            completion(.success(result?.user))
+        }
+    }
 }
